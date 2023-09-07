df651db5869bd7c64dee43b203ed840d2a5ff0e2
zaaiiin <zaainnaseem@gmail.com>
2023-09-07 09:14:21 +0800

    edit styles

--- app/imageGen.tsx
+++ app/imageGen.tsx
@@ -63,15 +63,15 @@ const ImageGenerator: React.FC<ImageGeneratorProps> = (props) => {
       titleElement.classList.add("title-animation");
     }
 
-    const aiImage = document.querySelector(props.aiImage);
-    if (aiImage) {
-      aiImage.classList.add("hidden");
-    }
+    // const aiImage = document.querySelector(props.aiImage);
+    // if (aiImage) {
+    //   aiImage.classList.add("hidden");
+    // }
   };
 
   return (
-    <div className="section flex">
-      {/* <div className="prompt-container flex py-10 items-center justify-center absolute top-3/4">
+    <div className="section flex flex-col w-full">
+      <div className="prompt-container flex  items-center justify-center w-full">
         <form onSubmit={handleSubmit}>
           <input
             type="text"
@@ -85,16 +85,17 @@ const ImageGenerator: React.FC<ImageGeneratorProps> = (props) => {
         <button type="submit" onClick={handleSubmit}>
           <Image src={searchbtn} alt="search" id="search-btn" />
         </button>
-      </div> */}
-      <div className="grid gap-8 image-container z-20 h-512 w-512  absolute top-1/4 border-zinc-800 border-2 border-opacity-50 rounded-md">
+      </div>
+
+      <div className="image-container flex justify-center z-20 border-2 border-opacity-50 rounded-md ">
         {image && (
-          <img src={image} alt="Generated-pic" className="image rounded-md" />
+          <img src={image} alt="Generated-pic" className="image rounded-md " />
         )}
       </div>
       <div className="loading-container flex absolute top-1/4 z-50">
         {isLoading && (
           <div>
-            <Skeleton className=" w-[512px] h-[514px]  flex justify-center my-auto" />
+            <Skeleton className=" w-[212px] h-[214px]  flex justify-center my-auto" />
             <div className="loading-indicator flex  justify-end font-pt-sans-narrow text-white tracking-widest uppercase scale-y-150 font-bold text-lg mt-2 "></div>
           </div>
         )}
--- app/page.tsx
+++ app/page.tsx
@@ -7,16 +7,16 @@ import AI from "../ai-image.png";
 
 export default function Home() {
   return (
-    <main className=" bg-black min-h-screen flex flex-col relative">
+    <main className=" bg-black min-h-screen flex flex-col">
       <Navbar />
 
       <div className="hero text-2xl md:text-4xl lg:text-7xl text-white w-full flex justify-center uppercase tracking-widest font-archivo-narrow font-bold leading-6">
-        <p className="title text-center leading-none transform absolute top-32 scale-y-150">
+        <p className="title text-center leading-none transform absolute top-28 scale-y-150">
           Generate <br></br>
           Stunning Visuals
         </p>
       </div>
-      <div className="ai-container flex justify-center my-auto absolute top-0.5 left-1/2">
+      <div className="ai-container flex flex-col w-60 justify-center items-center my-20 mx-auto z-20">
         <Image src={AI} alt="AI" id="ai-image1" className="ai-image" />
 
         <ImageGenerator title=".title" aiImage=".ai-container" />
