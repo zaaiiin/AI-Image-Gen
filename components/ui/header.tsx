7c38675fb52196013db7c5d9efab692ac8c450b8
zaaiiin <zaainnaseem@gmail.com>
2023-09-14 18:56:43 +0800

    edit styles of nav group

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -9,17 +9,17 @@ const Header = () => {
 
   return (
     <header className="max-w-[2440px]  mt-5 h-10 flex justify-between items-center sm:px-16 font-pt-sans-narrow relative">
-      <div className="logo absolute left-10 text-2xl text-lg flex items-center justify-center bg-gradient-to-r from-primary1 to-primary2 text-transparent bg-clip-text font-bold">
+      <div className="logo absolute left-10 text-lg flex items-center justify-center bg-gradient-to-r from-primary1 to-primary2 text-transparent bg-clip-text font-bold">
         PixaNova
       </div>
-      <nav className="navGroup  text-white text-sm  items-center absolute px-0 h-10 right-1/3 left-1/3 flex invisible text-center justify-center gap-5">
+      <nav className="navGroup invisible md:visible text-white text-sm items-center absolute px-0 h-10 right-1/4 left-1/4 flex  text-center justify-center gap-5">
         <div className="how ">How it Works</div>
         <div className="about">About Us</div>
         <div className="terms">Terms of Use</div>
       </nav>
 
       <button
-        className="menu absolute right-10 pt-2 z-120"
+        className="menu absolute right-10 pt-2 z-120 md:invisible"
         onClick={() => setToggle(!toggle)}
       >
         <Image
