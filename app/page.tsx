e766e5564d86725cc08a09b093805ced61cb9bb0
zaaiiin <zaainnaseem@gmail.com>
2023-09-14 10:19:02 +0800

    import Header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
-import Navbar from "./navbar";
-import ImageGenerator from "./imageGen";
-import AI from "../ai-image.png";
+import Header from "../components/ui/header";
+import ImageGenerator from "../components/services/imageGen";
+// import AI from "../ai-image.png";
 
 // import searchbtn from "../search-btn.png";
 // import { Textarea } from "@/components/ui/textarea";
@@ -9,7 +9,7 @@ import AI from "../ai-image.png";
 export default function Home() {
   return (
     <main className=" bg-black min-h-screen flex flex-col">
-      <Navbar />
+      <Header />
 
       <div className="hero text-2xl md:text-4xl lg:text-7xl text-white w-full flex justify-center uppercase tracking-widest font-archivo-narrow font-bold leading-6">
         <p className="title text-center leading-none transform absolute top-28 scale-y-150">
@@ -18,7 +18,14 @@ export default function Home() {
         </p>
       </div>
       <div className="ai-container flex flex-col w-60 justify-center items-center my-20 mx-auto z-20">
-        <Image src={AI} alt="AI" id="ai-image1" className="ai-image" />
+        <Image
+          src="/ai-image.png"
+          alt="AI"
+          id="ai-image1"
+          className="ai-image"
+          width="212"
+          height="212"
+        />
 
         <ImageGenerator title=".title" aiImage=".ai-container" />
       </div>
