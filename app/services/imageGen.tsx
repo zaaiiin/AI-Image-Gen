d482c532019891d7bc93d694802b0c92c5473331
zaaiiin <zaainnaseem@gmail.com>
2023-09-19 11:42:09 +0800

    edit async function

--- app/services/imageGen.tsx
+++ app/services/imageGen.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from "next/image";
 import React, { SyntheticEvent, useEffect, useState } from "react";
-import apiKey from "../../components/apiKey";
+// import apiKey from "../../components/apiKey";
 import { Skeleton } from "../../components/ui/skeleton";
 
 interface ImageGeneratorProps {
